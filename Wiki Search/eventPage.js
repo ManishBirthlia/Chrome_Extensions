8396b50afba4024caa946684333f1a5e302c2e78
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2023-10-01 19:27:16 +0530

    Add files via upload

---
+++ Wiki Search/eventPage.js
@@ -0,0 +1,24 @@
+var menuItem = {
+    "id": "WikiSearch",
+    "title": "WikitSearch",
+    "contexts": ["selection"]
+};
+
+chrome.contextMenus.create(menuItem);
+
+function fixedEncodeURI (str) {
+    return encodeURI(str).replace(/%5B/g, '[').replace(/%5D/g, ']');
+}
+
+chrome.contextMenus.onClicked.addListener(function(clickData){   
+    if (clickData.menuItemId == "WikiSearch" && clickData.selectionText){    
+        var wikiUrl = "https://en.wikipedia.org/wiki/" + fixedEncodeURI(clickData.selectionText);
+        var createData = {
+            "url": wikiUrl,
+            "type": "popup",
+            "top": 5,
+            "left": 5
+        };              
+        chrome.windows.create(createData, function(){});        
+    }
+});
